48ac0cf893460645f44307f14420c0031e74027d
Jesse Sheehan <jesse@sheehan.nz>
2022-01-19 21:39:09 +1300

    First commit, 2 images

---
+++ index.js
@@ -0,0 +1,56 @@
+const { createApi } = require("unsplash-js");
+const fetch = require("node-fetch");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+dotenv.config();
+
+const UNSPLASH_ACCESS_KEY = requireVariable("UNSPLASH_ACCESS_KEY");
+const UNSPLASH_COLLECTION_ID = requireVariable("UNSPLASH_COLLECTION_ID");
+
+async function main() {
+    const unsplash = createApi({
+        accessKey: UNSPLASH_ACCESS_KEY,
+        fetch
+    });
+
+    const photos = await getImageData(unsplash);
+    const html = fs.readFileSync("./src/index.html", { encoding: "utf-8"});
+
+    fs.writeFileSync(
+        "./docs/index.html",
+        html.replace("[/* PHOTO DATA GOES HERE */];", JSON.stringify(photos)),
+        { encoding: "utf-8" }
+    );
+}
+
+async function getImageData(unsplash) {
+    let response = await unsplash.collections.getPhotos({ collectionId: UNSPLASH_COLLECTION_ID });
+    if (response.type !== 'success') {
+        console.error("Response:", response);
+        throw new Error("An error occurred making a request to unsplash.");
+    }
+
+    const photos = response.response.results;
+
+    console.log(`Found ${photos.length} photos in collection.`);
+    
+    return photos
+        .map((photo) => ({
+            id: photo.id,
+            description: photo.description,
+            image_url: photo.urls.full,
+            user_name: photo.user.name ?? photo.user.username,
+            user_link: photo.user.links.html,
+        }));
+}
+
+function requireVariable(x) {
+    const value = process.env[x];
+    if (typeof value === "undefined") {
+        throw new Error(`This program requires the '${x}' environment variable to be set.`);
+    }
+    return value;
+}
+
+main();
